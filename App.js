6c6bac1ebaf3caa6f84a9e2ee6244c1c6e7dd6e0
wesleahlima <wesleyguedes7@gmail.com>
2021-07-28 13:42:11 -0300

    Aplicativo Busca CEP

--- App.js
+++ App.js
@@ -1,21 +1,110 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View
+} from 'react-native';
+import logo from './src/assets/logo.png';
+import api from './src/services/api'
 
 export default function App() {
+
+  const [CEP, setCEP] = useState('');
+  const [endereco, setEndereco] = useState([]);
+  const [logradouro, setLogradouro] = useState('');
+  const [bairro, setBairro] = useState('');
+  const [cidade, setCidade] = useState('');
+  const [estado, setEstado] = useState('');
+
+  const buscar = async () => {
+    const { data } = await api.get(`${CEP}/json/`)
+    setEndereco(data)
+    setLogradouro('Logradouro: ')
+    setBairro('Bairro: ')
+    setCidade('Estado: ')
+    setEstado('UF: ')
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.headerContainer}>
+        <Image source={logo} />
+        <Text style={styles.titulo}>Busca CEP</Text>
+        <TextInput style={styles.input}
+          keyboardType="number-pad"
+          maxLength={8}
+          onChangeText={txtCep => setCEP(txtCep)}
+          placeholder="Digite o CEP que deseja buscar"
+          placeholderTextColor="#fff"
+        />
+        <TouchableOpacity style={styles.botao}
+          onPress={buscar}>
+          <Text style={styles.textoBotao}>BUSCAR</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.enderecoContainer}>
+        <Text style={styles.texto}>{logradouro} {endereco.logradouro}</Text>
+        <Text style={styles.texto}>{bairro} {endereco.bairro}</Text>
+        <Text style={styles.texto}>{cidade} {endereco.localidade}</Text>
+        <Text style={styles.texto}>{estado} {endereco.uf}</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  botao: {
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#F6E125',
+    borderColor: '#F6E125',
+    borderRadius: 5,
+    borderWidth: 1,
+    fontSize: 16,
+    height: 40,
+    marginTop: 20,
+    padding: 8,
+    width: 300,
   },
+  container: {
+    alignItems: 'center',
+    backgroundColor: '#2F48D4',
+    flex: 1,
+    paddingTop: 50,
+  },
+  enderecoContainer: {
+    alignItems: 'center',
+    marginTop: 30,
+  },
+  headerContainer: {
+    alignItems: 'center',
+  },
+  input: {
+    borderColor: '#fff',
+    borderRadius: 5,
+    borderWidth: 1,
+    color: '#fff',
+    fontSize: 16,
+    height: 40,
+    marginTop: 20,
+    padding: 8,
+    textAlign: 'center',
+    width: 300,
+  },
+  texto: {
+    color: '#fff',
+    fontSize: 20,
+  },
+  textoBotao: {
+    color: '#2F48D4',
+    fontSize: 17,
+    fontWeight: 'bold',
+  },
+  titulo: {
+    color: '#F6E125',
+    fontSize: 30,
+    fontWeight: 'bold'
+  }
 });
---
+++ README.md
@@ -0,0 +1,3 @@
+# Aplicativo ​Busca CEP :mobile_phone_off:
+
+Aplicativo mobile desenvolvido em React Native utilizando o cliente HTTP Axios para buscar o CEP na API viacep.com.br.
