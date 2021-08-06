f57dc4590da1e0638a84cbc0f48cbd3485b4fc17
wesleahlima <wesleyguedes7@gmail.com>
2021-08-06 16:33:12 -0300

    Implementação da Status Bar

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import {
   Alert,
   Image,
+  StatusBar,
   StyleSheet,
   Text,
   TextInput,
@@ -12,10 +13,8 @@ import logo from './src/assets/logo.png';
 import api from './src/services/api'
 
 export default function App() {
-
   const [CEP, setCEP] = useState('');
   const [endereco, setEndereco] = useState([]);
-
   const buscar = () => {
     api
       .get(`${CEP}/json`)
@@ -27,6 +26,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+      <StatusBar backgroundColor="#2F48D4" barStyle="light-content"  />
       <View style={styles.headerView}>
         <Image source={logo} />
         <Text style={styles.titulo}>Busca CEP</Text>
@@ -35,7 +35,7 @@ export default function App() {
           maxLength={8}
           onChangeText={txtCep => setCEP(txtCep)}
           placeholder="Digite o CEP que deseja buscar"
-          placeholderTextColor="#fff"
+          placeholderTextColor="#2F48D4"
         />
         <TouchableOpacity style={styles.botao}
           onPress={buscar}>
@@ -53,6 +53,31 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: '#2F48D4',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  headerView: {
+    alignItems: 'center',
+  },
+  titulo: {
+    color: '#F6E125',
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+  input: {
+    backgroundColor: '#FFF',
+    borderRadius: 5,
+    color: '#2F48D4',
+    fontSize: 16,
+    height: 40,
+    marginTop: 20,
+    padding: 8,
+    textAlign: 'center',
+    width: 300,
+  },
   botao: {
     alignItems: 'center',
     backgroundColor: '#F6E125',
@@ -65,43 +90,17 @@ const styles = StyleSheet.create({
     padding: 8,
     width: 300,
   },
-  container: {
-    alignItems: 'center',
-    backgroundColor: '#2F48D4',
-    flex: 1,
-    paddingTop: 50,
-  },
-  enderecoView: {
-    alignItems: 'center',
-    marginTop: 30,
-  },
-  headerView: {
-    alignItems: 'center',
-  },
-  input: {
-    borderColor: '#fff',
-    borderRadius: 5,
-    borderWidth: 1,
-    color: '#fff',
-    fontSize: 16,
-    height: 40,
-    marginTop: 20,
-    padding: 8,
-    textAlign: 'center',
-    width: 300,
-  },
-  texto: {
-    color: '#fff',
-    fontSize: 20,
-  },
   textoBotao: {
     color: '#2F48D4',
     fontSize: 17,
     fontWeight: 'bold',
   },
-  titulo: {
-    color: '#F6E125',
-    fontSize: 30,
-    fontWeight: 'bold'
-  }
+  enderecoView: {
+    alignItems: 'center',
+    marginTop: 30,
+  },
+  texto: {
+    color: '#fff',
+    fontSize: 20,
+  },
 });
