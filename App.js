218136bb772b3abfb0f670bb321258e4049dcda8
wesleahlima <wesleyguedes7@gmail.com>
2021-08-10 20:39:56 -0300

    Mudança para a API fetch

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import {
   Alert,
   Image,
+  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
@@ -10,44 +11,50 @@ import {
   View
 } from 'react-native';
 import logo from './src/assets/logo.png';
-import api from './src/services/api'
 
 export default function App() {
   const [CEP, setCEP] = useState('');
   const [endereco, setEndereco] = useState([]);
+
   const buscar = () => {
-    api
-      .get(`${CEP}/json`)
-      .then(({ data }) => {
-        setEndereco(data)
-      })
-      .catch((error) => Alert.alert('CEP Inválido', 'Digite um CEP válido.'))
+    fetch(`https://viacep.com.br/ws/${CEP}/json/`)
+      .then((response) => response.json())
+      .then((json) => setEndereco(json))
+      .catch((error) => Alert.alert(error))
   };
 
   return (
     <View style={styles.container}>
-      <StatusBar backgroundColor="#2F48D4" barStyle="light-content"  />
-      <View style={styles.headerView}>
-        <Image source={logo} />
-        <Text style={styles.titulo}>Busca CEP</Text>
-        <TextInput style={styles.input}
-          keyboardType="number-pad"
-          maxLength={8}
-          onChangeText={txtCep => setCEP(txtCep)}
-          placeholder="Digite o CEP que deseja buscar"
-          placeholderTextColor="#2F48D4"
-        />
-        <TouchableOpacity style={styles.botao}
-          onPress={buscar}>
-          <Text style={styles.textoBotao}>BUSCAR</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.enderecoView}>
-        <Text style={styles.texto}>{endereco.logradouro}</Text>
-        <Text style={styles.texto}>{endereco.bairro}</Text>
-        <Text style={styles.texto}>{endereco.localidade}</Text>
-        <Text style={styles.texto}>{endereco.uf}</Text>
-      </View>
+      <ScrollView>
+        <StatusBar backgroundColor="#2F48D4" barStyle="light-content"  />
+        <View style={styles.headerView}>
+          <Image source={logo} />
+          <Text style={styles.titulo}>Busca CEP</Text>
+          <TextInput style={styles.input}
+            keyboardType="number-pad"
+            maxLength={8}
+            onChangeText={txtCep => setCEP(txtCep)}
+            placeholder="Digite o CEP que deseja buscar"
+            placeholderTextColor="#2F48D4"
+          />
+          <TouchableOpacity style={styles.botao}
+            onPress={buscar}>
+            <Text style={styles.textoBotao}>BUSCAR</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.enderecoView}>
+          <Text style={styles.texto}>CEP: {endereco.cep}</Text>
+          <Text style={styles.texto}>Logradouro: {endereco.logradouro}</Text>
+          <Text style={styles.texto}>Complemento: {endereco.complemento}</Text>
+          <Text style={styles.texto}>Bairro: {endereco.bairro}</Text>
+          <Text style={styles.texto}>Localidade: {endereco.localidade}</Text>
+          <Text style={styles.texto}>UF: {endereco.uf}</Text>
+          <Text style={styles.texto}>IBGE: {endereco.ibge}</Text>
+          <Text style={styles.texto}>GIA: {endereco.gia}</Text>
+          <Text style={styles.texto}>DDD: {endereco.ddd}</Text>
+          <Text style={styles.texto}>SIAFI: {endereco.siafi}</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -100,7 +107,8 @@ const styles = StyleSheet.create({
     marginTop: 30,
   },
   texto: {
-    color: '#fff',
+    color: '#F6E125',
     fontSize: 20,
+    fontWeight: 'bold',
   },
 });
